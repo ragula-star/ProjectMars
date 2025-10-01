97a9a1b437cdfc9831ade9a8f6c19fa5228a3eaa
ragula-star <ragula6@gmail.com>
2025-10-01 09:27:14 +0530

    Update ShareskillPages.cs

--- Pages/ShareskillPages.cs
+++ Pages/ShareskillPages.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ProjectMars.Pages
-{
-    internal class ShareskillPages
-    {
-    }
-}
+//namespace ProjectMars.Pages
+//{
+  //  internal class ShareskillPages
+    //{
+    //}
+//}
